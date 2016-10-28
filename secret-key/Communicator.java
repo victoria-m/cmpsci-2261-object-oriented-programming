3fc3724e8bbcc37f6c84a6d04e3acbcf74b0b104
Victoria Miltcheva <victoriamiltcheva@users.noreply.github.com>
2016-10-28 16:28:25 -0500

    Create Communicator.java

---
+++ secret-key/Communicator.java
@@ -0,0 +1,21 @@
+package SecretKey;
+
+public class Communicator {
+   private String name = "";
+   
+   public Communicator() {
+      
+   }
+   
+   public Communicator(String name) {
+      this.name = name;
+   }
+   
+   public void setName(String name) {
+      this.name = name;
+   }
+   
+   public String getName() {
+      return this.name;
+   }
+}
