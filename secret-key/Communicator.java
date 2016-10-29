a523d926ebe4c843c57aa932c207c6db3bf73b0b
Victoria Miltcheva <victoriamiltcheva@users.noreply.github.com>
2016-10-28 21:02:45 -0500

    add new functions, variables, and constructors

--- secret-key/Communicator.java
+++ secret-key/Communicator.java
@@ -1,19 +1,45 @@
+package SecretKey;
+
 public class Communicator {
+
    private String name = "";
-   
-   public Communicator() {
-      
+   private StringBuilder message = new StringBuilder("");
+   private StringBuilder secretKey = new StringBuilder();
+
+   Communicator() { }
+
+   Communicator(String name, StringBuilder message, StringBuilder secretKey) {
+      this.name = name;
+      this.message = message;
+      this.secretKey = secretKey;
    }
-   
-   public Communicator(String name) {
+
+   Communicator(String name) {
+      this(name, new StringBuilder(""), new StringBuilder(""));
+   }
+
+   void setName(String name) {
       this.name = name;
    }
-   
-   public void setName(String name) {
-      this.name = name;
-   }
-   
-   public String getName() {
+
+   String getName() {
       return this.name;
    }
+
+   void setText(StringBuilder message) {
+      this.message = message;
+   }
+
+   StringBuilder getText() {
+      return this.message;
+   }
+
+   void setSecretKey(StringBuilder secretKey) {
+      this.secretKey = secretKey;
+   }
+
+   StringBuilder getSecretKey() {
+      return this.secretKey;
+   }
+
 }
