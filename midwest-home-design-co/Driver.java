b64c5dc5a22b68ef4d8303caabb9c4f3d1614d07
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-11-19 15:27:59 -0600

    Create Driver.java

---
+++ midwest-home-design-co/Driver.java
@@ -0,0 +1,18 @@
+
+public class Driver {
+   public static void main(String[] args) {
+      
+      // ask customer to select the design style preferences
+      // and specifications
+      
+      // software asks customer to:
+         // enter home style
+         // total area range
+         // num beds/baths
+      // customer should enter AT LEAST home style
+
+      // customer can customize the house design templates
+      // make sure to calculate the total design costs
+      // and display all that to the customer
+   }
+}
