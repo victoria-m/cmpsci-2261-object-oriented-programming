d72486048c4819d4b739cd90db49d50ee27dfa04
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-11-20 14:06:47 -0600

    Create Customizable.java

---
+++ midwest-home-design-co/Customizable.java
@@ -0,0 +1,6 @@
+interface Customizable {
+   
+   // allows users to edit the default house templates or their own custom house template
+   void customize(String style, int numBedrooms, int numBathrooms, double totalArea);
+   
+}
