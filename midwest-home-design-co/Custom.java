9a8a3eceed638919ed276d65e79cbebe57fde1cf
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-11-20 18:12:28 -0600

    Modify default constructor

--- midwest-home-design-co/Custom.java
+++ midwest-home-design-co/Custom.java
@@ -1,8 +1,8 @@
 
-//for creating custom houses
 public class Custom extends House {
 
-   protected Custom(String style, double numBedrooms, double numBathrooms, double totalArea, double templateRate) {
-      super(style, numBedrooms, numBathrooms, totalArea, templateRate);
+   // used for creating custom houses
+   protected Custom(String style, double numBedrooms, double numBathrooms, double totalArea) {
+      super(style, numBedrooms, numBathrooms, totalArea);
    }
 }
