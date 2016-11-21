59c3272ce161a3c9a36c5f6455477531710dc7ff
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-11-20 18:18:34 -0600

    Simplify constructor and fix calcTotalCost()

--- midwest-home-design-co/Traditional.java
+++ midwest-home-design-co/Traditional.java
@@ -4,18 +4,18 @@ public class Traditional extends House {
    // default template properties
    private final static String DEFAULT_STYLE = "Traditional";
    private final static int DEFAULT_NUM_BEDROOMS = 3, DEFAULT_NUM_BATHROOMS = 2;
-   private final static double TEMPLATE_BASIC_RATE = 3300, DEFAULT_TOTAL_AREA = 2200;
+   private final static double DEFAULT_BASIC_RATE = 3300, DEFAULT_TOTAL_AREA = 2200;
 
    // default constructor
    protected Traditional() {
-      super(DEFAULT_STYLE, DEFAULT_NUM_BEDROOMS, DEFAULT_NUM_BATHROOMS, DEFAULT_TOTAL_AREA, TEMPLATE_BASIC_RATE);
+      super(DEFAULT_STYLE, DEFAULT_NUM_BEDROOMS, DEFAULT_NUM_BATHROOMS, DEFAULT_TOTAL_AREA);
    }
    
    // re-implement method so that it uses the default values of Traditional
    @Override
    protected double calcTotalCost() {
       
-      double totalCost, templateBasicRate = this.getTemplateRate();
+      double totalCost, templateBasicRate = DEFAULT_BASIC_RATE;
       double extraBedrooms = 0, extraBathrooms = 0;
 
       // if there are extra beds/baths
@@ -25,9 +25,14 @@ public class Traditional extends House {
       if (this.getNumBathrooms() > DEFAULT_NUM_BATHROOMS)
          extraBathrooms = this.getNumBathrooms() - DEFAULT_NUM_BATHROOMS;
 
+      
+      System.out.println(this.getTotalArea());
+      
       // multiply basic rate by 1.5 if the total area is >= 3,000 sq ft
-      if (this.getTotalArea() >= 3000)
+      if (this.getTotalArea() >= 3000) {
          templateBasicRate *= 1.5;
+         System.out.println("basic rate is now " + templateBasicRate);
+      }
 
       // calculate total cost
       totalCost = (templateBasicRate + ((800 * extraBedrooms) + (500 * extraBathrooms)));
