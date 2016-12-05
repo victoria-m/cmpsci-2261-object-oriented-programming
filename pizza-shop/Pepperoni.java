bb61fff5803d6a0196bee4e36a2e719c0ecda211
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-05 12:53:19 -0600

    Create Pepperoni.java

---
+++ pizza-shop/Pepperoni.java
@@ -0,0 +1,11 @@
+
+public class Pepperoni extends Pizza {
+   
+   final static double COSTPERSLICE = 2.25;
+   final static String TYPE = "Cheese";
+   
+   protected Pepperoni() {
+      super(COSTPERSLICE, TYPE);
+   }
+   
+}
