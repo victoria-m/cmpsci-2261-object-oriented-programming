f75371051ffbea64d6e41a26d1ad1fb41fdaae43
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-05 12:53:32 -0600

    Create Vegetable.java

---
+++ pizza-shop/Vegetable.java
@@ -0,0 +1,11 @@
+
+public class Vegetable extends Pizza {
+   
+   final static double COSTPERSLICE = 2.00;
+   final static String TYPE = "Vegetable";
+   
+   protected Vegetable() {
+      super(COSTPERSLICE, TYPE);
+   }
+
+}
