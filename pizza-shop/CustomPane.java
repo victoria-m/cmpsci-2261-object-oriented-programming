f5ea1091a58b7f67a4eab6134a3b0c7e980d8d7b
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-13 01:58:54 -0600

    Create CustomPane.java

---
+++ pizza-shop/CustomPane.java
@@ -0,0 +1,15 @@
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+
+class CustomPane extends StackPane {
+   
+   public CustomPane(String title) {
+      
+      getChildren().add(new Label(title));
+      setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+   
+   }
+   
+}
