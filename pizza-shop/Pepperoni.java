517682d0d1e35950453dc1b2bdd51d9cc20684e3
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-16 21:17:00 -0600

    Update Pepperoni.java

--- pizza-shop/Pepperoni.java
+++ pizza-shop/Pepperoni.java
@@ -1,11 +1,12 @@
+package PizzaShop;
 
 public class Pepperoni extends Pizza {
-   
+
    final static double COSTPERSLICE = 2.25;
    final static String TYPE = "Cheese";
-   
+
    protected Pepperoni() {
       super(COSTPERSLICE, TYPE);
    }
-   
+
 }
