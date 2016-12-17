022d642b79d0dec2d7002dd5edd106edda317037
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-16 21:16:30 -0600

    Update Cheese.java

--- pizza-shop/Cheese.java
+++ pizza-shop/Cheese.java
@@ -1,11 +1,12 @@
+package PizzaShop;
 
 public class Cheese extends Pizza {
-   
+
    final static double COSTPERSLICE = 1.50;
    final static String TYPE = "Cheese";
-   
+
    protected Cheese() {
       super(COSTPERSLICE, TYPE);
    }
-   
+
 }
