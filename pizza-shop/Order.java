cccdf3fbaf10369dc1c5cffcb0cff1dd913b42ef
Victoria M <victoriamiltcheva@users.noreply.github.com>
2016-12-16 21:16:44 -0600

    Update Order.java

--- pizza-shop/Order.java
+++ pizza-shop/Order.java
@@ -1,13 +1,16 @@
+package PizzaShop;
+
+import java.util.List;
 
 // contains information about the customer's order
 
 public class Order {
    protected double totalCost;
-   
+
    protected Order() {
       this.totalCost = 0;
    }
-   
+
    public double getTotalCost() {
       return totalCost;
    }
@@ -15,13 +18,20 @@ public class Order {
    public void setTotalCost(double totalCost) {
       this.totalCost = totalCost;
    }
-   
+
    protected void addToTotalCost(double amount) {
       this.totalCost += amount;
    }
-   
-   static void calculateCost() {
-      
+
+   // takes into account every slice selected and its cost
+   double calculateCost(List<Integer> numSlicesPerPizza, List<Double> costPerSlice) {
+
+      double totalCost = 0;
+
+      for (int i = 0; i < numSlicesPerPizza.size(); ++i)
+         totalCost += (numSlicesPerPizza.get(i) * costPerSlice.get(i));
+
+      return totalCost;
    }
-   
+
 }
