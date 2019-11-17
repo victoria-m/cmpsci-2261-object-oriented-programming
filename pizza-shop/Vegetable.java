644cac81fb36098cbc6d9a8c78900be68d16605c
Victoria Miltcheva <12804086+victoria-miltcheva@users.noreply.github.com>
2019-11-17 14:33:14 -0500

    Update Vegetable.java

--- pizza-shop/Vegetable.java
+++ pizza-shop/Vegetable.java
@@ -1,12 +1,10 @@
 package PizzaShop;
 
 public class Vegetable extends Pizza {
-
    final static double COSTPERSLICE = 2.00;
    final static String TYPE = "Vegetable";
 
    protected Vegetable() {
       super(COSTPERSLICE, TYPE);
    }
-
 }
